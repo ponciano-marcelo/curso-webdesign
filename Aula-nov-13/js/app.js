4dff5de3157f79ef136c65f2fdf4356471663289
Marcelo Ponciano <ponciano.marcelo@eb.mil.br>
2020-11-16 16:05:25 -0300

    acerto da aula 13

--- Aula-nov-13/js/app.js
+++ Aula-nov-13/js/app.js
@@ -4,13 +4,10 @@ var img = new Image();
 var indexImagem=0;
 var url = 'imgs/';
 var slider = document.getElementById('slider');
-var barra = document.getElementById('barra');
-var tempoTroca=0;
 
 function iniciar(){
     preCarregamento();
     carregarImagem(indexImagem);
-    anima();
 }
 
 function preCarregamento() {
@@ -28,7 +25,6 @@ function carregarImagem(indexImagem) {
 function troca (inc) {
 
     indexImagem+=inc;
-    tempoTroca=0;
 
     if(indexImagem>imagens.length-1){
         indexImagem=0;
@@ -40,20 +36,5 @@ function troca (inc) {
 
     carregarImagem(indexImagem);
 }
-
-function anima() {
-    tempoTroca++
-
-    if(tempoTroca >=500){
-        tempoTroca=0;
-        troca(1);
-    }
-
-    vtempo=tempoTroca/5;
-    barra.style.width=vtempo + "%";
-    window.requestAnimationFrame(anima);
-
-}
-
 window.addEventListener("load", iniciar );
 
