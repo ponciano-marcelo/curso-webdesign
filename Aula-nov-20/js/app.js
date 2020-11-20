bbbc6ef2224fa98258b8d3871def6d67859eca1d
Marcelo Ponciano <ponciano.marcelo@eb.mil.br>
2020-11-20 19:33:53 -0300

    Inlusao do carrossel, mudança do menu e ajustes no layout da pagina

--- Aula-nov-20/js/app.js
+++ Aula-nov-20/js/app.js
@@ -1,36 +1,123 @@
- var botao = document.querySelector('.js-botao');
 
-botao.onclick = function() {
-    menu = document.querySelector('.js-menu');
-    menu.classList.toggle('menu--display');
-} 
+/*------------------*/
+/*      MENU        */
+/*------------------*/
 
+var App = App || {};
 
-// var EstadoBotao = (function(){
+App.EstadoBotao = (function(){
 
-//     function EstadoBotao() {
-//         this.botao = document.querySelector('.js-botao');
-//         this.menu = document.querySelector('.js-menu');
-//     }
+    function EstadoBotao() {
+        this.botao = $('.js-botao');
+        this.menu = $('.js-menu');
+        this.emitter = $({});
+		this.on = this.emitter.on.bind(this.emitter);
+    }
 
-//     EstadoBotao.prototype.iniciar = function(){
+    EstadoBotao.prototype.iniciar = function(){
 
-//          this.botao.onclick = onBotaoPressionado.bind(this); 
+         this.botao.on('click', onBotaoPressionado.bind(this)); 
 
-//     }
+    }
 
-//     function onBotaoPressionado() {
+    function onBotaoPressionado() {
 
-//         this.menu = document.querySelector('.js-menu');
-//         this.menu.classList.toggle('menu--display');
+        this.menu[0].classList.toggle('menu--display');
 
-//     }
+    }
 
-//     return EstadoBotao;
+    return EstadoBotao;
 
-// })();
+})();
 
+/*------------------*/
+/*     CARROSSEL    */
+/*------------------*/
 
-// var estadoBotao = new EstadoBotao();
+App.Carrossel = (function(){
 
-// estadoBotao.iniciar();
+    function Carrossel() {
+    	this.nomeImagens = ['cadeira.jpg','sofa.jpg','televisao.jpg'];
+		this.imagens = [];
+		this.img = new Image();
+		this.indexImagem=0;
+		this.url = 'img/';
+		this.slider = $('#slider');
+		this.barra = $('#barra');
+		this.btnSliderMais = $('#js-btn-slider-mais');
+		this.btnSliderMenos = $('#js-btn-slider-menos');
+		this.tempoTroca=0;
+    }
+
+    Carrossel.prototype.iniciar = function(){
+    	this.btnSliderMenos.on('click', onBtnSliderMenosClicado.bind(this));
+    	this.btnSliderMais.on('click', onBtnSliderMaisClicado.bind(this));
+		preCarregamento.call(this);
+    	carregarImagem.call(this);
+    	anima.call(this);
+    }
+
+    function onBtnSliderMenosClicado () {
+    	troca.call(this,-1);
+    }
+	
+    function onBtnSliderMaisClicado () {
+    	troca.call(this,1);
+    }
+
+	function preCarregamento() {
+
+	    for(nome of this.nomeImagens){
+	        this.imagens.push(this.img.src = this.url + nome);
+	    }
+	}
+
+	function carregarImagem() {
+
+	    imagem = this.imagens[this.indexImagem]
+	    texto = 'url("'+ imagem +'")';
+	    this.slider.css('backgroundImage', texto);
+	}
+
+	function troca (inc) {
+
+	    this.indexImagem+=inc;
+	    this.tempoTroca=0;
+
+	    if(this.indexImagem>this.imagens.length-1){
+	        this.indexImagem=0;
+	    }
+
+	    if(this.indexImagem<0){
+	        this.indexImagem=this.imagens.length-1;
+	    }
+
+	    carregarImagem.call(this,this.indexImagem);
+	}
+
+	function anima() {
+	    this.tempoTroca++;
+
+	    if(this.tempoTroca >=500){
+	        this.tempoTroca=0;
+	        troca.call(this,1);
+	    }
+
+	    this.vtempo=this.tempoTroca/5;
+	    this.barra.css('width', this.vtempo + '%');
+	    window.requestAnimationFrame(anima.bind(this));
+	}
+
+    return Carrossel;
+
+})();
+
+$(function() {
+	
+	var estadoBotao = new App.EstadoBotao();
+	estadoBotao.iniciar();
+
+	var carrossel = new App.Carrossel();
+	carrossel.iniciar();
+	
+});
