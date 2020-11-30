ac76f67973e1ed4b4c48557b1b76d21b567245d0
Marcelo Ponciano <ponciano.marcelo@eb.mil.br>
2020-11-30 15:33:18 -0300

    Criacao inicial dos componentes

--- Aula-nov-30/projeto/src/components/Header.js
+++ Aula-nov-30/projeto/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default class App extends React.Component {
+export default class Header extends React.Component {
     render () {
         return (
             <div>
-              <h1>Olá Mundo</h1>
+              <h1>Header</h1>
             </div>
         );
     }
